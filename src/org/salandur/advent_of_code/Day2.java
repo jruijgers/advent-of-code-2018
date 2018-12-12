bfb25cc3d0417792b8a69e75a2c02ee892c311bc
Jeroen Rubis-Ruijgers <jeroen.ruijgers@jdriven.com>
2018-12-12 14:10:19 +0100

    Find the boxes with only 1 letter different and print their matching letters

--- src/org/salandur/advent_of_code/Day2.java
+++ src/org/salandur/advent_of_code/Day2.java
@@ -1,8 +1,12 @@
 package org.salandur.advent_of_code;
 
+import org.apache.commons.text.similarity.LevenshteinDetailedDistance;
+import org.apache.commons.text.similarity.LevenshteinResults;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +22,9 @@ public class Day2 {
         long containsLettersThreeTimes = containsLettersThreeTimes(result);
 
         System.out.println("Day 2.1: " + containsLettersThreeTimes * containsLettersTwice);
+
+        List<String> boxesWithFabrics = findBoxesWithFabrics(Files.readAllLines(day2));
+        System.out.println("Day 2.2: " + getMatchingLetters(boxesWithFabrics.get(0), boxesWithFabrics.get(1)));
     }
 
     private static Map<Character, Integer> characterCounters(String string) {
@@ -33,6 +40,34 @@ public class Day2 {
 
     private static long containsLettersThreeTimes(List<Map<Character, Integer>> result) {
         return result.stream().filter(v -> v.values().contains(3)).count();
+    }
+
+    private static List<String> findBoxesWithFabrics(List<String> boxes) {
+        for (int i = 1; i < boxes.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (containFabric(boxes.get(i), boxes.get(j))) {
+                    return List.of(boxes.get(i), boxes.get(j));
+                }
+            }
+        }
+        return Collections.emptyList();
+    }
+
+    private static boolean containFabric(String box1, String box2) {
+        LevenshteinResults result = LevenshteinDetailedDistance.getDefaultInstance().apply(box1, box2);
+
+        return result.getSubstituteCount() == 1 && result.getDeleteCount() == 0 && result.getInsertCount() == 0;
+    }
+
+    private static String getMatchingLetters(String box1, String box2) {
+        StringBuilder b = new StringBuilder(box1.length());
+
+        for (int i = 0; i < box1.length(); i++) {
+            if (box1.charAt(i) == box2.charAt(i)) {
+                b.append(box1.charAt(i));
+            }
+        }
+        return b.toString();
     }
 }
 
