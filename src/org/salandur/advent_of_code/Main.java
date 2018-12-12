abe9e62ae2c24af80f39f72be7ba7e565dc4a35e
Jeroen Rubis-Ruijgers <jeroen.ruijgers@jdriven.com>
2018-12-12 14:11:38 +0100

    Add general main to call it for each day

---
+++ src/org/salandur/advent_of_code/Main.java
@@ -0,0 +1,10 @@
+package org.salandur.advent_of_code;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Day1.main(args);
+        Day2.main(args);
+    }
+}
