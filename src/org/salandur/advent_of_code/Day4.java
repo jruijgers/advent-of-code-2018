c3c54b02fc5566dc0cad6872a2ef0fda4288ec01
Jeroen Rubis-Ruijgers <jeroen.ruijgers@jdriven.com>
2018-12-13 16:09:12 +0100

    Day 4.2: find time a guard slept the most (code is getting uglier...)

--- src/org/salandur/advent_of_code/Day4.java
+++ src/org/salandur/advent_of_code/Day4.java
@@ -3,9 +3,7 @@ package org.salandur.advent_of_code;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Function;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -24,16 +22,57 @@ public class Day4 {
 
         List<GuardShift> allShifts = Files.lines(data).sorted().collect(Day4::new, Day4::accept, Day4::combine).shifts;
 
-        Map<Integer, List<GuardShift>> guards = allShifts.stream().collect(groupingBy(GuardShift::getGuardNumber));
-        Map<Integer, Integer> totalSleepPerGuard = guards.entrySet().stream().collect(toMap(Map.Entry::getKey, Day4::totalGuardSleep));
+        Map<Guard, List<GuardShift>> guards = allShifts.stream().collect(groupingBy(GuardShift::getGuard));
+        Map<Guard, Integer> totalSleepPerGuard = guards.entrySet().stream().collect(toMap(Map.Entry::getKey, Day4::totalGuardSleep));
 
-        Integer mostSleepingGuard = findKeyWithMax(totalSleepPerGuard);
+        Guard mostSleepingGuard = findKeyWithMax(totalSleepPerGuard);
         Map<Integer, Integer> guardSleepsOnMinute = sleepPerMinute(guards.get(mostSleepingGuard));
 
         Integer sleepsTheMostAt = findKeyWithMax(guardSleepsOnMinute);
+        Integer timesAsleep = guardSleepsOnMinute.get(sleepsTheMostAt);
 
-        System.out.printf("Day 4.1: Guard #%d sleeps the most, at 00:%02d, %d times. Answer is %d\n", mostSleepingGuard, sleepsTheMostAt, guardSleepsOnMinute.get(sleepsTheMostAt), mostSleepingGuard * sleepsTheMostAt);
+        System.out.printf("Day 4.1: Guard %s sleeps the most, at 00:%02d, %d times. Answer is %d\n", mostSleepingGuard, sleepsTheMostAt, timesAsleep, mostSleepingGuard.getId() * sleepsTheMostAt);
+
+        Map<Guard, Map<Integer, Integer>> guardSleepPerMinute = guards.entrySet().stream().collect(toMap(Map.Entry::getKey, Day4::sleepPerMinute));
+        Map<Integer, Map<Guard, Integer>> minuteGuardsSleep = guardSleepPerMinute.entrySet().stream().flatMap(Day4::toMinuteAndGuard).collect(toMap(Map.Entry::getKey, Day4::guardMinuteValueMapper, Day4::combineGuardMinutes));
+        Optional<Map.Entry<Integer, Map<Guard, Integer>>> guardsSleepsTheMostOnMinute = minuteGuardsSleep.entrySet().stream().reduce(Day4::reduceGuardsSleep);
+
+        mostSleepingGuard = findKeyWithMax(guardsSleepsTheMostOnMinute.get().getValue());
+        timesAsleep = guardsSleepsTheMostOnMinute.get().getValue().get(mostSleepingGuard);
+        sleepsTheMostAt = guardsSleepsTheMostOnMinute.get().getKey();
+
+        System.out.printf("Day 4.2: Guard %s sleeps the most at 00:%02d, %d times. Answer is %d\n", mostSleepingGuard, sleepsTheMostAt, timesAsleep, mostSleepingGuard.getId() * sleepsTheMostAt);
     }
+
+    private static Map.Entry<Integer, Map<Guard, Integer>> reduceGuardsSleep(Map.Entry<Integer, Map<Guard, Integer>> x, Map.Entry<Integer, Map<Guard, Integer>> y) {
+        Integer maxX = x.getValue().get(findKeyWithMax(x.getValue()));
+        Integer maxY = y.getValue().get(findKeyWithMax(y.getValue()));
+
+        return maxX > maxY ? x : y;
+    }
+
+    private static Map<Guard, Integer> guardMinuteValueMapper(Map.Entry<Integer, Map<Guard, Integer>> t) {
+        return t.getValue();
+    }
+
+    private static Map<Guard, Integer> combineGuardMinutes(Map<Guard, Integer> u, Map<Guard, Integer> u1) {
+        for (Map.Entry<Guard, Integer> e:u1.entrySet()) {
+            u.put(e.getKey(), e.getValue());
+        }
+        return u;
+    }
+
+    private static Stream<Map.Entry<Integer, Map<Guard, Integer>>> toMinuteAndGuard(Map.Entry<Guard, Map<Integer, Integer>> entry) {
+        Map<Integer, Map<Guard, Integer>> result = new HashMap<>();
+
+        for (Map.Entry<Integer, Integer> x : entry.getValue().entrySet()) {
+            result.putIfAbsent(x.getKey(), new HashMap<>());
+            result.get(x.getKey()).put(entry.getKey(), x.getValue());
+        }
+
+        return result.entrySet().stream();
+    }
+
 
     private static <T> T findKeyWithMax(Map<T, Integer> map) {
         return map.entrySet().stream().max((l, r) -> l.getValue() - r.getValue()).get().getKey();
@@ -59,8 +98,12 @@ public class Day4 {
         }
     }
 
-    private static int totalGuardSleep(Map.Entry<Integer, List<GuardShift>> guardShifts) {
+    private static int totalGuardSleep(Map.Entry<Guard, List<GuardShift>> guardShifts) {
         return guardShifts.getValue().stream().mapToInt(gs -> gs.sleepingMinutes.size()).sum();
+    }
+
+    private static Map<Integer, Integer> sleepPerMinute(Map.Entry<Guard, List<GuardShift>> guardShifts) {
+        return sleepPerMinute(guardShifts.getValue());
     }
 
     private static Map<Integer, Integer> sleepPerMinute(List<GuardShift> guardShifts) {
@@ -79,22 +122,47 @@ public class Day4 {
         shifts.add(lastShift);
     }
 
+    private static class Guard {
+        private final Integer id;
+
+        public Guard(String id) {
+            this.id = Integer.valueOf(id);
+        }
+
+        public Integer getId() {
+            return id;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Guard guard = (Guard) o;
+            return Objects.equals(id, guard.id);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id);
+        }
+
+        public String toString() {
+            return "#" + id;
+        }
+    }
+
     private static class GuardShift {
-        private final Integer guardNumber;
+        private final Guard guard;
         private List<Integer> sleepingMinutes = new ArrayList<>();
 
         private Integer lastFallAsleep = null;
 
         public GuardShift(String guardNumber) {
-            this.guardNumber = Integer.valueOf(guardNumber);
+            this.guard = new Guard(guardNumber);
         }
 
-        public Integer getGuardNumber() {
-            return guardNumber;
-        }
-
-        public List<Integer> getSleepingMinutes() {
-            return sleepingMinutes;
+        public Guard getGuard() {
+            return guard;
         }
 
         public void fallAsleep(Integer minute) {
@@ -107,7 +175,7 @@ public class Day4 {
 
         @Override
         public String toString() {
-            return "GuardShift[#" + guardNumber + "," + sleepingMinutes.size() + "]";
+            return "GuardShift[" + guard + "," + sleepingMinutes.size() + "]";
         }
     }
 }
