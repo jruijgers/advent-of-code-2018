7f71221cad82754c34297d29626b4dcd7cebbccd
Jeroen Rubis-Ruijgers <jeroen.ruijgers@jdriven.com>
2018-12-14 09:37:05 +0100

    Day 5: find the fully contracted polymer

--- src/org/salandur/advent_of_code/Day5.java
+++ src/org/salandur/advent_of_code/Day5.java
@@ -1,7 +1,29 @@
 package org.salandur.advent_of_code;
 
-public class Day5 {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.LinkedList;
 
+public class Day5 {
+    public static void main(String[] args) throws IOException {
+        Path data = Path.of("day5.txt");
+
+        String basePolymer = Files.readAllLines(data).get(0);
+
+        LinkedList<Character> chars = new LinkedList<>();
+
+        basePolymer.chars().forEach(currentChar -> {
+            char previousChar = chars.size() == 0 ? 0 : chars.peek();
+
+            if (currentChar != previousChar && Character.toLowerCase(currentChar) == Character.toLowerCase(previousChar)) {
+                // we have the same char, in different case
+                chars.pop();
+            } else {
+                chars.push((char) currentChar);
+            }
+        });
+
+        System.out.printf("Day 5.1: the polymer contracts to a length of %d\n", chars.size());
     }
 }
